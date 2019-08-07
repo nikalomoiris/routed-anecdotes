3bb80e23e8f533559f62354b0fa756a9097b15b8
nikalomiris <nikalomiris@gmail.com>
2019-08-07 05:31:28 -0400

    Part 7 routed anecdotes step 2 solution

--- src/App.js
+++ src/App.js
@@ -21,8 +21,19 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <div>has {anecdote.votes} votes</div>
+    <div>for more info see {anecdote.info}</div>
   </div>
 )
 
@@ -135,6 +146,9 @@ const App = () => {
           <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
           <Route path="/about" render={() => <About />} />
           <Route path="/create" render={() => <CreateNew addNew={addNew} />} />
+          <Route exact path="/anecdotes/:id" render={({ match }) =>
+            <Anecdote anecdote={anecdoteById(match.params.id)} />
+          } />
         </div>
       </Router>
       <Footer />
